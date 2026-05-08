e2977db1377ad6ca9f415f794daf9ba87e4ac2c4
vadiraj <cognizant.raj@daimlertruck.com>
2026-05-08 12:38:56 +0530

    added spring cloud load balancer client side

---
+++ student-service/src/main/java/com/univeristy/ms/feignclients/AddressServiceLoadBalanceConfig.java
@@ -0,0 +1,18 @@
+package com.univeristy.ms.feignclients;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+
+import feign.Feign;
+
+@LoadBalancerClient(value = "address-service")
+public class AddressServiceLoadBalanceConfig {
+	
+	@LoadBalanced
+	@Bean
+	public Feign.Builder feignBuilder(){
+		return Feign.builder();
+	}
+
+}
